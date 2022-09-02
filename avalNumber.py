c0e2add1773abf3c70da4593bb57eb24a25bbcd1
Iman Mir <12563322+mir5@users.noreply.github.com>
2022-09-02 18:33:54 +0430

    Fist project

--- avalNumber.py
+++ avalNumber.py
@@ -1,1 +1,36 @@
-print("Hel lo")
+def is_prime(number):
+ if number < 2:
+    return False
+
+ if number == 2:
+    return True
+
+ if number % 2 == 0:
+    return False
+
+ for i in range(3, int(number**0.5), 2):
+    if number % i == 0:
+        return False
+ 
+ return True
+
+def calculate_prime_divisors(number):
+ primes = []
+
+ for i in range(2, number+1):
+    if ((is_prime(i)) and (number % i == 0)):
+        primes.append(i)
+ 
+ return primes
+
+numbers = {}
+for i in range(10):
+ n = int(input())
+ numbers[n] = len(calculate_prime_divisors(n))
+
+# find maximum number of prime divisors
+max_value = max(numbers.values())
+# find numbers with max_value number of prime divisors
+max_keys = [k for k, v in numbers.items() if v == max_value]
+
+print( sorted(max_keys)[-1], max_value)
